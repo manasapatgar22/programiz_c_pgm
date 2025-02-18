9bd927be45d64de8eec8cb32c387eed45e3562f9
Manasa Mohan Patgar <mohanpatgar120@gmail.com>
2025-02-18 19:27:37 +0530

    Update C Program to Add Two Integers.c

--- C Program to Add Two Integers.c
+++ C Program to Add Two Integers.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 void main()
 {
-    int number1,number2,result;
+    int number1,number2,sum;
     printf("Enter two number to add: ");
     scanf("%d %d",&number1,&number2);
-    result = number1 + number2;
+    sum = number1 + number2;
     printf("%d + %d = %d",number1,number2,result);
 }
